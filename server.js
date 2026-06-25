3aab4311a4ecf0c8804755accfe5ea793bb39766
Najeeb863 <99027763+Najeeb863@users.noreply.github.com>
2026-06-25 14:44:41 +0500

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname)));
+
+io.on('connection', (socket) => {
+    console.log('User connected: ' + socket.id);
+
+    socket.on('send_message', (data) => {
+        io.emit('receive_message', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
